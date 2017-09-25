4b48dd193c1f4f00710ddc5d73f033b3ff69cce4
Alessio-Palumbo <alessio.palumbo4@gmail.com>
2017-09-25 15:03:31 +1000

    Add validation to artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,8 @@
 class Artist < ApplicationRecord
+  validates :name, presence: true
+
+  validates :bio, presence: true, length: { maximum: 8000 }
+  # Same as above but longer
+  # validates_presence_of :bio
+  # validates_length_of :bio, maximum: 8000, too_long: ': You must not exceed 8000 characters!'
 end
